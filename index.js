930061730ae78555632208d0460e64772791b8d4
light-200 <aryanshaily123@gmail.com>
2025-03-14 05:01:31 +0530

    feat: eslint and prettier setup

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/**
- * @format
- */
-// // Only import react-native-gesture-handler on native platforms
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
